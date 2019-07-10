916b13d828417d746cf33c4ab6e218e2f0c3dd24
尹凯 <suoyiguke_yinkai@aliyun.com>
2019-07-10 14:35:06 +0800

    修改配置

--- scan_mian.py
+++ scan_mian.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-import json
-import random
-import requests
 import sys
 import time
 from threading import Thread, Lock
+
+import requests
 from cffi import lock
 from selenium.webdriver.chrome.options import Options
-from client.py_cli import ProxyFetcher
 
 import logger
+from client.py_cli import ProxyFetcher
 
 log = logger.logger()
 sys.setrecursionlimit(1000000)
@@ -43,19 +42,16 @@ global linkSet
 linkSet = set()
 
 if 'scan_url' in os.environ and 'scan_access_token' in os.environ:
-    pageURL = os.environ['popularize_url']
     access_token = os.environ['popularize_access_token']
     object['keyword'] = os.environ['popularize_keyword']
 else:
     with open('./config.yml', 'r', encoding="utf-8") as f:
         ymlFile = yaml.load(f.read(), Loader=yaml.FullLoader)
-        pageURL = ymlFile['popularize_url']
         access_token = ymlFile['popularize_access_token']
         object['keyword'] = ymlFile['popularize_keyword']
 
 # # 初始化机器人小丁
-webhook = 'https://oapi.dingtalk.com/robot/send?access_token={access_token}'.format(
-    access_token=access_token)  # 填写你自己创建的机器人
+webhook = 'https://oapi.dingtalk.com/robot/send?access_token={access_token}'.format(access_token=access_token)  # 填写你自己创建的机器人
 xiaoding = DingtalkChatbot(webhook)
 
 
@@ -87,26 +83,16 @@ def _remove_duplicate(dict_list):
 
 # 点击所有链接
 def send_url_verification(url, browser):
-    # 只扫描自己的域名
-    if url.find(pageURL) != -1:
-        ym = coverHttps(url)
-        browser.get(ym)
-        aList = browser.find_elements_by_css_selector("a")
-        for a in aList:
-            if a.text.find('游戏攻略')!=-1:
-                a.click()
-                time.sleep(2)
-
-        # for a in aList:
-        #     href_ = a.attrs['href']
-        #     if href_ and href_[0:1] != '#':
-        #         linkSet.add(has_ym(href_))
-        #
-        # for src in linkSet:
-        #     if src not in historySet:
-        #         send_url_verification(src,browser)
-        #         historySet.add(ym)
-        #
+    # # 只扫描自己的域名
+    # if url.find(url) != -1:
+    #     ym = coverHttps(url)
+    #     browser.get(ym)
+    #     aList = browser.find_elements_by_css_selector("a")
+    #     for a in aList:
+    #         if a.text.find('游戏攻略')!=-1:
+    #             a.click()
+    #             time.sleep(2)
+    print("暂时没有实现点击所有网站链接！")
 
 
 def checkIp(IP):
@@ -149,7 +135,7 @@ def getBrowser():
     chrome_options = Options()
     # chrome_options.add_argument('--headless')
     # chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--proxy-server=' + getPxIp())
+    # chrome_options.add_argument('--proxy-server=' + getPxIp())
     try:
         browser = webdriver.Remote(
             command_executor="http://chrome-scan:4444/wd/hub",
@@ -168,14 +154,14 @@ def getBrowser():
 
 def sendDingDing(bl, keyword):
     if bl:
-        print('检测到关键词: “' + keyword + '”在百度的第' + object['page'] + '页,第' + object['article '] + '条')
-        # xiaoding.send_text(msg='检测到关键词: “' + keyword + '”在百度的第'+ object['page'] +'页,第'+object['article ']+'条')
+        # print('检测到关键词: “' + keyword + '”在百度的第' + object['page'] + '页,第' + object['article '] + '条')
+        xiaoding.send_text(msg='检测到关键词: “' + keyword + '”在百度的第'+ object['page'] +'页,第'+object['article ']+'条')
     else:
-        print('检测到关键词: “' + keyword + '”没有被百度收录')
-        # xiaoding.send_text(msg='检测到关键词: “' + keyword + '”没有被百度收录')
+        # print('检测到关键词: “' + keyword + '”没有被百度收录')
+        xiaoding.send_text(msg='检测到关键词: “' + keyword + '”没有被百度收录')
 
 
-def threadSend(keyword):
+def threadSend(url,keyword):
     if 'page' not in object:
         browser = getBrowser()
         time.sleep(2)
@@ -187,17 +173,17 @@ def threadSend(keyword):
             time.sleep(1)
             browser.find_element_by_css_selector('#su').click()
             time.sleep(1)
-            resolution_page(browser.page_source, browser,keyword)
+            resolution_page(url,browser.page_source, browser,keyword)
 
         except BaseException as err:
             browser.quit()
             log.error(err)
-            threadSend(keyword)
+            threadSend(url,keyword)
 
         browser.quit()
 
 
-def resolution_page(text, browser,keyword):
+def resolution_page(url,text, browser,keyword):
     soup = BeautifulSoup(text, 'lxml')
     selects = soup.select("div[class='result c-container']")
     i = 0
@@ -206,7 +192,7 @@ def resolution_page(text, browser,keyword):
         i += 1
         aList = div.select("a[class='c-showurl']")
         for a_ in aList:
-            if a_.text.find(pageURL) != -1:
+            if a_.text.find(url) != -1:
                 print('找到了！')
                 page = soup.select_one("span[class='fk fk_cur']").find_next_sibling().text
                 print('第' + page + '页')
@@ -217,17 +203,13 @@ def resolution_page(text, browser,keyword):
                 lock.release()
 
                 # 推送订订消息
-                selectors = browser.find_elements_by_css_selector('.n')
-                if len(selectors) == 0 or selectors[1].text.find('下一页') == -1:
-                  sendDingDing(False, keyword)
-                else:
-                  sendDingDing(True, keyword)
+                sendDingDing(True, keyword)
 
                 # 点击进入官网
                 num = (int(page) - 1) * 10 + i
                 browser.find_element_by_id(str(num)).find_element_by_css_selector("a").click()
                 time.sleep(2)
-                send_url_verification(pageURL, browser)
+                send_url_verification(url, browser)
 
                 break
         else:
@@ -237,27 +219,38 @@ def resolution_page(text, browser,keyword):
     # 没找到就点击下一页继续
     else:
         print('下一页....')
+
         selectors = browser.find_elements_by_css_selector('.n')
+        flag = True
         for selector_ in selectors:
             if selector_.text.find('下一页') != -1:
+                flag = False
                 time.sleep(1)
                 selector_.click()
                 time.sleep(1)
-                resolution_page(browser.page_source, browser,keyword)
+                resolution_page(url,browser.page_source, browser,keyword)
+
+        #没有下一页了，就是没找到
+        # 推送订订消息
+        if flag:
+            sendDingDing(False, keyword)
 
 
 def main():
     # 初始化ip
-    initIP();
+    # initIP();
     log.debug('=================扫描程序开始==========================')
     # 线程list
     threads = []
-    keywordList = object['keyword'].split(",")
+    keywordList = object['keyword'].split("|")
 
     for keyword in keywordList:
-        p = Thread(target=threadSend, args=(keyword,))
-        p.start()
-        threads.append(p)
+        aList = keyword.split("=")
+        bList = aList[len(aList)-1].split(",")
+        for bKey in bList:
+            p = Thread(target=threadSend, args=(aList[0],bKey))
+            p.start()
+            threads.append(p)
 
     for t in threads:
         t.join()
